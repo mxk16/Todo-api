3281323bf7d309ef368fb1affb8f438cbf619324
Maxim Kloushin <makr1975@yandex.ru>
2016-12-26 19:31:36 +0100

    Add ?completed filter

--- server.js
+++ server.js
@@ -13,9 +13,18 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
@@ -89,4 +98,21 @@ app.put('/todos/:id', function (req, res) {
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
